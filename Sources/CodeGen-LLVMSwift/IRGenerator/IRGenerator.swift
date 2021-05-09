14148a25dffb443e6f286f7c4624b1568c7ff00c
Chirag Ramani <chirag.ramani7@gmail.com>
2021-05-10 01:37:49 +0530

    Added IRGenerator

--- Sources/CodeGen-LLVMSwift/IRGenerator/IRGenerator.swift
+++ Sources/CodeGen-LLVMSwift/IRGenerator/IRGenerator.swift
@@ -11,11 +11,103 @@ import LLVM
 final class IRGenerator {
     
     private let ast: AST
+    let module: Module
     private let builder: IRBuilder
+    private var localVariables = [String: IRValue]()
     
     init(ast: AST) {
         self.ast = ast
-        let module = Module(name: "main")
+        self.module = Module(name: "main")
         self.builder = IRBuilder(module: module)
+    }
+    
+    
+    func emitIR() throws {
+        try ast.nodes.forEach { node in
+            switch node {
+            case .functionDeclaration(let functionDeclaration):
+                try emitFunctionDeclaration(functionDeclaration)
+            default: fatalError()
+            }
+        }
+    }
+    
+    // MARK: Private
+    
+    private func emitFunctionDeclaration(_ functionDeclaration: FunctionDeclaration) throws {
+        let mainFunction = builder.addFunction(functionDeclaration.name,
+                                               type: .init([], VoidType()))
+        let entry = mainFunction.appendBasicBlock(named: "entry")
+        builder.positionAtEnd(of: entry)
+        if let bodyExpr = functionDeclaration.body as? FunctionBodyExpression {
+            try bodyExpr.body.forEach { try self.emitExpr($0) }
+        }
+        builder.buildRetVoid()
+    }
+    
+   @discardableResult
+    private func emitExpr(_ expr: Expr) throws -> IRValue {
+        if let assignmentExpression = expr as? AssignmentExpression {
+            return try emitAssignmentExpression(assignmentExpression)
+        } else if let integerExpression = expr as? IntegerExpression {
+            return LLVM.IntType.int32.constant(integerExpression.intValue)
+        } else if let printStatement = expr as? PrintStatement {
+            return try emitPrintStatement(printStatement)
+        } else {
+            fatalError()
+        }
+    }
+    
+    private func emitPrintStatement(_ expr: PrintStatement) throws -> IRValue {
+        let printFunction = emitPrintf()
+        guard let argumentType = expr.value.first else { return printFunction }
+        switch argumentType {
+        case .propertyReference(let propertyReadExpression):
+            let variableReferenced =
+            builder.buildLoad(localVariables[propertyReadExpression.name]!,
+                              type: LLVM.IntType.int32) // Remove int32 hardcoding
+            return builder.buildCall(printFunction,
+                                     args: [formatSpecifier(of: variableReferenced),
+                                            variableReferenced])
+        default:
+            return printFunction
+        }
+    }
+    
+    private func emitAssignmentExpression(_ expr: AssignmentExpression) throws -> IRValue {
+        let value = try emitExpr(expr.value)
+        let variableName = expr.name
+        let localVariable = builder.buildAlloca(type: LLVM.IntType.int32,
+                                                name: variableName)
+        let storedRef = builder.buildStore(value,
+                                           to: localVariable)
+        localVariables[variableName] = localVariable
+        return storedRef
+    }
+    
+    private func emitPrintf() -> Function {
+       if let function = module.function(named: "printf") { return function }
+       
+        let printfType = FunctionType([PointerType(pointee: IntType.int8)], IntType.int32, variadic: true)
+       return builder.addFunction("printf", type: printfType)
+     }
+    
+    private func formatSpecifier(of value: IRValue) -> IRValue {
+        if let _ = (value.type as? LLVM.IntType) {
+            return builder.buildGlobalStringPtr("%d\n",
+                                                name: "PRINTF_INTEGER")
+        }
+        fatalError()
+    }
+}
+
+private extension AssignmentExpression {
+    var name: String {
+        switch lhs {
+        case .constant(let constantDeclaration):
+            return constantDeclaration.name
+        case .variable(let variableDeclaration):
+            return variableDeclaration.name
+        }
     }
 }
--- Sources/CodeGen-LLVMSwift/main.swift
+++ Sources/CodeGen-LLVMSwift/main.swift
@@ -1,18 +1,39 @@
 import Foundation
 
-let intermediateRepresentations: [IRGen] = [
-    FunctionReturningSum().generateIR()
-]
-
 let code = """
  func main() {
  let a = 10
  print(a)
 }
 """
+
 let lexer = Lexer(input: code)
 let tokenKinds = lexer.lex()
 
 let parser = Parser(tokenKinds: tokenKinds)
 let ast = try parser.parse()
-print(ast)
+let irGenerator = IRGenerator(ast: ast)
+
+try? irGenerator.emitIR()
+try? irGenerator.module.verify()
+
+print(irGenerator.module.dump())
+
+/*
+; ModuleID = 'main'
+source_filename = "main"
+
+@PRINTF_INTEGER = private unnamed_addr constant [4 x i8] c"%d\0A\00", align 1
+
+define void @main() {
+entry:
+  %a = alloca i32, align 4
+  store i32 10, i32* %a, align 4
+  %0 = load i32, i32* %a, align 4
+  %1 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @PRINTF_INTEGER, i32 0, i32 0), i32 %0)
+  ret void
+}
+
+declare i32 @printf(i8* %0, ...)
+*/
+
